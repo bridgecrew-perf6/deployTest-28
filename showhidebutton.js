82f18a405829322e86045db321fed94ca3e0df2a
Al3h4andr0 <76854850+Al3h4andr0@users.noreply.github.com>
2022-04-03 22:48:43 +0300

    first commit

---
+++ showhidebutton.js
@@ -0,0 +1,16 @@
+function show(){
+    document.getElementById('content').style.height="50%"
+    document.getElementById('content').style.display="block"
+    document.getElementById('show').style.display="block"
+    document.getElementById('show').style.top="2%"
+    document.getElementById('content').style.top="9%"
+
+}
+
+function hide(){
+    document.getElementById('content').style.height="0px"
+    document.getElementById('content').style.display="none"
+    document.getElementById('show').style.display="inline"
+}
+
+
